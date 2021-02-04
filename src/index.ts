bf5404c9c7393a7c7f303878c2b6c8726fa68367
Alan Fitzpatrick <alajfit@gmail.com>
2021-02-04 18:28:05 +0000

    Little bit further

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,50 @@
 class DQ<T> {
-    private _list: T | null[]
-    private _size: number
-    private _arrayLength: number
-    private _start: number
-    private _end: number
+    private _holderArray: (T | null)[]
+    private _floatingArraySize: number
+    private _startPointer: number
+    private _endPointer: number
 
     constructor(length: number = 0) {
-        this._list = length > 0 ? new Array(length).fill(null) : []
-        this._start = this._end = length > 0 ? Math.floor(length / 2) : 0
-        this._arrayLength = length
-        this._size = 0
+        this._holderArray = length > 0 ? new Array(length).fill(null) : []
+        this._startPointer = this._endPointer = length > 0 ? Math.ceil(length / 2) - 1 : 0
+        this._floatingArraySize = 0
     }
 
     size() {
-        return this._size
+        return this._floatingArraySize
     }
 
     add(value: T) {
-        if (this._arrayLength > this._size) {
-            if (this._end === this._arrayLength) {
-                this._list[0] = value
-                this._end = 0
+        if (this._holderArray.length > this._floatingArraySize) {
+            if (this._endPointer === this._holderArray.length) {
+                this._holderArray[0] = value
+                this._endPointer = 0
             } else {
-                this._list[this._end + 1] = value
-                this._end = this._end + 1
+                this._holderArray[this._endPointer] = value
+                this._endPointer = this._endPointer + 1
             }
+        } else if (this._holderArray.length) {
+            const holderArray = new Array(this._holderArray.length * 2).fill(null)
+            let newStart = Math.floor(this._holderArray.length * 0.25)
+
+            console.log(this._startPointer, this._endPointer)
+            // while (this._startPointer !== this._endPointer) {
+            //     holderArray[newStart] = this._holderArray[this._startPointer]
+            //     newStart++
+            //     this._startPointer = this._startPointer === this._holderArray.length - 1 ? 0 : this._startPointer + 1
+            // }
+
+            this._startPointer = newStart - this._holderArray.length
+            this._endPointer = newStart
+            this._holderArray = holderArray
         } else {
-            // Double Array in Size
-            // Add old array to new array
-            // Drop reference to old array allowing for Garbage Collection
+            this._holderArray = new Array(3).fill(null)
+            this._startPointer = 1
+            this._endPointer = 2
+            this._holderArray[this._startPointer] = value
         }
+        this._floatingArraySize++
+        console.log(this)
     }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -11,23 +11,18 @@ describe("JavaScript DequeueArray Data Structure", () => {
         expect(dequeueArray.size()).toBe(0)
     })
 
-    it ("Should allow us to add an element to the end of the DequeueArray", () => {
-        expect(dequeueArray.size()).toBe(1)
+    it ("Should allow us to add an element to the end of the DequeueArray with a preset length", () => {
+        dequeueArray = new DQ(10)
+        dequeueArray.add('10')
+        dequeueArray.add('20')
+        expect(dequeueArray.size()).toBe(2)
     })
 
-    // it("Should allow the user to add items to the front of DequeueArray", () => {
-    //     expect(dequeueArray.enqueue()).toBe(2)
-    // })
-
-    // it("Should allow the user to add items to the back of DequeueArray", () => {
-
-    // })
-
-    // it("Should allow the user to dequeue from the DequeueArray", () => {
-
-    // })
-
-    // it("Should allow the user to deshift items from the DequeueArray", () => {
-
-    // })
+    it ("Should allow us to add an element to the end of the DequeueArray with no preset length", () => {
+        dequeueArray.add('10')
+        dequeueArray.add('20')
+        dequeueArray.add('30')
+        dequeueArray.add('99')
+        expect(dequeueArray.size()).toBe(2)
+    })
 })
